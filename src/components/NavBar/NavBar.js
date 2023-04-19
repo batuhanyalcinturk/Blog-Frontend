8555e17f7785989462a618988aa650ecb6fe803e
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-04-19 16:03:22 +0300

    frontend güncellemeleri

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function NavBar(){
+export default function NavBar() {
     let userId = 5;
 
-    return(
+    return (
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to={{pathname : '/users/' + userId}}>User</Link></li>
-            </ul>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                        
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/">Home</Link>
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    <Link to={{ pathname: '/users/' + userId }}>User</Link>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
-
-export default NavBar;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,47 @@
 import React from "react";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton';
 import "./Post.scss";
+import { Avatar, CardHeader } from "@mui/material";
+import { red } from "@mui/material/colors";
 
-function Post(props){
+export default function Post(props) {
 
-    const {title, text} = props;
+    const { title, text } = props;
 
-    return(
+    return (
         <div className="postContainer">
-            {title}
-            {text}
+            <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {text}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <Button size="small">Share</Button>
+                    <Button size="small">Devamını Oku</Button>
+                </CardActions>
+            </Card>
         </div>
     )
 }
-
-export default Post;
